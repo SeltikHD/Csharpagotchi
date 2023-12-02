e526afe54030314d6986f3e094e605825199a7be
Ewerthon Kawan <ewerthonkawan@gmail.com>
2023-12-02 15:11:52 -0300

    Adicionando lógica do game

--- Csharpagotchi/MainWindow.xaml.cs
+++ Csharpagotchi/MainWindow.xaml.cs
@@ -1,28 +1,99 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Csharpagotchi
 {
+    // Componentes
+    public class PositionComponent
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+
+    public class VelocityComponent
+    {
+        public double SpeedX { get; set; }
+        public double SpeedY { get; set; }
+    }
+
+    // Entidade
+    public class Entity
+    {
+        public List<object> Components { get; set; } = new List<object>();
+    }
+
+    // Sistema
+    public class MovementSystem
+    {
+        public void Update(List<Entity> entities)
+        {
+            foreach (var entity in entities)
+            {
+                if (entity.Components.FirstOrDefault(c => c is PositionComponent) is PositionComponent positionComponent && entity.Components.FirstOrDefault(c => c is VelocityComponent) is VelocityComponent velocityComponent)
+                {
+                    // Atualiza a posição com base na velocidade
+                    positionComponent.X += velocityComponent.SpeedX;
+                    positionComponent.Y += velocityComponent.SpeedY;
+
+                    // Aqui você poderia adicionar lógica de colisão, limites da tela, etc.
+                }
+            }
+        }
+    }
+
     /// <summary>
     /// Interação lógica para MainWindow.xam
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly DispatcherTimer gameTimer;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Inicializa o DispatcherTimer
+            gameTimer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(1000 / 60) // Define o intervalo para aproximadamente 60 FPS
+            };
+            gameTimer.Tick += GameLoop; // Define o método a ser chamado a cada tick
+            gameTimer.Start(); // Inicia o temporizador
+        }
+
+        private void GameLoop(object sender, EventArgs e)
+        {
+            // Lógica do jogo - atualizações, movimentos, cálculos, etc.
+            // Chamadas de métodos para atualizar a lógica do jogo
+
+            // Por exemplo:
+            AtualizarPosicoes();
+            VerificarColisoes();
+
+            // Atualização da interface gráfica, se necessário
+            // Atualizações de elementos visuais com base nos estados do jogo
+
+            // Por exemplo:
+            AtualizarInterface();
+        }
+
+        // Métodos de exemplo para atualizar a lógica do jogo e a interface gráfica
+        private void AtualizarPosicoes()
+        {
+            // Lógica para atualizar as posições dos elementos do jogo
+        }
+
+        private void VerificarColisoes()
+        {
+            // Lógica para verificar colisões entre objetos do jogo
+        }
+
+        private void AtualizarInterface()
+        {
+            // Lógica para atualizar a interface gráfica com base no estado do jogo
         }
     }
 }

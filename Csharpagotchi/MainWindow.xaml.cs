f465e311a85ee360826140817319546657b5a333
Ewerthon Kawan <ewerthonkawan@gmail.com>
2023-12-03 17:10:24 -0300

    Slime andando, pulando e tentando tocar música

--- Csharpagotchi/MainWindow.xaml.cs
+++ Csharpagotchi/MainWindow.xaml.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-using System.IO;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Threading;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using WpfAnimatedGif;
 
 namespace Csharpagotchi
 {
@@ -21,24 +23,43 @@ namespace Csharpagotchi
     {
         public double SpeedX { get; set; }
         public double SpeedY { get; set; }
+        public double DistanceToChangeDirection { get; set; }
     }
 
     public class SpriteComponent
     {
+        // Objeto contendo as Uri dos sprites do personagem parado e andando
         public Uri Sprite { get; set; }
-        public UIElement UIElement { get; set; }
+        public Image Image { get; set; }
+        public bool IsMoving { get; set; }
+        public bool PreviousIsMoving { get; set; }
     }
 
     // Classe para o áudio
     public class AudioManager
     {
-        private readonly MediaPlayer mediaPlayer = new MediaPlayer();
+        public readonly MediaPlayer mediaPlayer = new MediaPlayer();
         private readonly Random random = new Random();
 
-        public void PlayRandomSound(string folderPath)
+        private static string GetAbsolutePathFromRelativeUri(Uri relativeUri)
+        {
+            // Verificar se esse código está executando no Visual Studio ou em um executável
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // Se estiver executando no Visual Studio, retorna o caminho absoluto com duas pastas acima
+                return Path.GetFullPath("../../" + relativeUri.ToString());
+            }
+            else
+            {
+                // Se estiver não estiver executando no Visual Studio, retorna o caminho absoluto
+                return Path.GetFullPath(relativeUri.ToString());
+            }
+        }
+
+        public void PlayRandomSound(Uri folderPath)
         {
             // Obtém a lista de arquivos de áudio na pasta
-            string[] audioFiles = Directory.GetFiles(folderPath, "../assets/slime/sounds/*.wav");
+            string[] audioFiles = Directory.GetFiles(GetAbsolutePathFromRelativeUri(folderPath), "*.wav");
 
             if (audioFiles.Length > 0)
             {
@@ -64,23 +85,63 @@ namespace Csharpagotchi
 
     public class EntityFactory
     {
+        private static string GetAbsolutePathFromRelativeUri(string relativeUri)
+        {
+            // Verificar se esse código está executando no Visual Studio ou em um executável
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // Se estiver executando no Visual Studio, retorna o caminho absoluto com duas pastas acima
+                return Path.GetFullPath("../../" + relativeUri);
+            }
+            else
+            {
+                // Se estiver não estiver executando no Visual Studio, retorna o caminho absoluto
+                return Path.GetFullPath(relativeUri);
+            }
+        }
+
         public Entity CreateSlime(double x, double y)
         {
             Entity slime = new Entity();
 
             slime.Components.Add(new PositionComponent { X = x, Y = y });
             slime.Components.Add(new VelocityComponent { SpeedX = 0, SpeedY = 0 });
-            slime.Components.Add(new SpriteComponent { Sprite = new Uri("../assets/slime/default.png", UriKind.Relative), UIElement = new Image() });
+            slime.Components.Add(new SpriteComponent { Sprite = new Uri(GetAbsolutePathFromRelativeUri("./assets/slime/sprites/walk150.gif")) });
 
             return slime;
         }
-    }   
+    }
 
     // Sistema
-    public class MovementSystem
+    interface ISystem
+    {
+        void Start(List<Entity> entities, Canvas canvas);
+        void Update(List<Entity> entities);
+    }
+
+    public class MovementSystem : ISystem
     {
         private readonly AudioManager audioManager = new AudioManager();
-        
+        private readonly Random random = new Random();
+        private Canvas canvas;
+
+        public void Start(List<Entity> entities, Canvas _canvas)
+        {
+            canvas = _canvas;
+            foreach (var entity in entities)
+            {
+                if (entity.Components.FirstOrDefault(c => c is PositionComponent) is PositionComponent positionComponent)
+                {
+                    if (entity.Components.FirstOrDefault(c => c is VelocityComponent) is VelocityComponent velocityComponent)
+                    {
+                        // Define a velocidade inicial ao iniciar o jogo
+                        velocityComponent.DistanceToChangeDirection = 0;
+                        SetRandomVelocity(velocityComponent);
+                    }
+                }
+            }
+        }
+
         public void Update(List<Entity> entities)
         {
             foreach (var entity in entities)
@@ -91,17 +152,102 @@ namespace Csharpagotchi
                     positionComponent.X += velocityComponent.SpeedX;
                     positionComponent.Y += velocityComponent.SpeedY;
 
-                    // Tocar audio de movimento
-                    audioManager.PlayRandomSound("../assets/slime/sounds/");
+                    // Toca áudio aleatório de movimento
+                    bool isPlaying = audioManager.mediaPlayer.Position < audioManager.mediaPlayer.NaturalDuration;
+                    if (!isPlaying)
+                    {
+                        audioManager.PlayRandomSound(new Uri("./assets/slime/sounds/", UriKind.Relative));
+                    }
 
-                    // Aqui você poderia adicionar lógica de colisão, limites da tela, etc.
+                    // Verifica se o slime está prestes a sair da tela
+                    CheckScreenBounds(positionComponent, velocityComponent, canvas);
+
+                    // Atualiza a velocidade aleatoriamente
+                    SetRandomVelocity(velocityComponent);
+
+                    // Verificar se o slime está parado ou andando
+                    if (velocityComponent.SpeedX == 0 && velocityComponent.SpeedY == 0)
+                    {
+                        ((SpriteComponent)entity.Components.FirstOrDefault(c => c is SpriteComponent)).IsMoving = false;
+                    }
+                    else
+                    {
+                        ((SpriteComponent)entity.Components.FirstOrDefault(c => c is SpriteComponent)).IsMoving = true;
+                    }
                 }
             }
         }
+
+        private void CheckScreenBounds(PositionComponent positionComponent, VelocityComponent velocityComponent, Canvas canvas)
+        {
+            // Obtém as dimensões da tela
+            double screenWidth = canvas.ActualWidth;
+            double screenHeight = canvas.ActualHeight;
+
+            // Obtém as dimensões do sprite
+            double spriteWidth = 98;
+            double spriteHeight = 98;
+
+            // Verifica se o slime está fora dos limites da tela e ajusta a posição
+            if (positionComponent.X < 0)
+            {
+                positionComponent.X = 0;
+                velocityComponent.SpeedX = Math.Abs(velocityComponent.SpeedX); // Inverte a direção
+            }
+            else if (positionComponent.X + spriteWidth > screenWidth)
+            {
+                positionComponent.X = screenWidth - spriteWidth;
+                velocityComponent.SpeedX = -Math.Abs(velocityComponent.SpeedX); // Inverte a direção
+            }
+
+            if (positionComponent.Y < 0)
+            {
+                positionComponent.Y = 0;
+                velocityComponent.SpeedY = Math.Abs(velocityComponent.SpeedY); // Inverte a direção
+            }
+            else if (positionComponent.Y + spriteHeight > screenHeight)
+            {
+                positionComponent.Y = screenHeight - spriteHeight;
+                velocityComponent.SpeedY = -Math.Abs(velocityComponent.SpeedY); // Inverte a direção
+            }
+        }
+
+        private void SetRandomVelocity(VelocityComponent velocityComponent)
+        {
+            if (velocityComponent.DistanceToChangeDirection <= 0)
+            {
+                // Gera velocidades aleatórias
+                velocityComponent.SpeedX = random.NextDouble() * 4 - 1; // Valor entre -1 e 1
+                velocityComponent.SpeedY = random.NextDouble() * 4 - 1; // Valor entre -1 e 1
+
+                // Gera distância aleatória para mudar a direção
+                velocityComponent.DistanceToChangeDirection = random.NextDouble() * 1000;
+            }
+            else
+            {
+                velocityComponent.DistanceToChangeDirection -= Math.Abs(velocityComponent.SpeedX) < Math.Abs(velocityComponent.SpeedY) ? Math.Abs(velocityComponent.SpeedY) : Math.Abs(velocityComponent.SpeedX);
+            }
+        }
+
     }
 
-    public class RenderingSystem
+    public class RenderingSystem : ISystem
     {
+        private static string GetAbsolutePathFromRelativeUri(string relativeUri)
+        {
+            // Verificar se esse código está executando no Visual Studio ou em um executável
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // Se estiver executando no Visual Studio, retorna o caminho absoluto com duas pastas acima
+                return Path.GetFullPath("../../" + relativeUri);
+            }
+            else
+            {
+                // Se estiver não estiver executando no Visual Studio, retorna o caminho absoluto
+                return Path.GetFullPath(relativeUri);
+            }
+        }
+
         public void Start(List<Entity> entities, Canvas canvas)
         {
             foreach (var entity in entities)
@@ -109,7 +255,13 @@ namespace Csharpagotchi
                 if (entity.Components.FirstOrDefault(c => c is SpriteComponent) is SpriteComponent spriteComponent)
                 {
                     // Adiciona o sprite ao canvas
-                    canvas.Children.Add(spriteComponent.UIElement);
+                    spriteComponent.Image = new Image
+                    {
+                        Source = new BitmapImage(spriteComponent.Sprite),
+                        Height = 98,
+                        Width = 98
+                    };
+                    canvas.Children.Add(spriteComponent.Image);
                 }
             }
         }
@@ -121,14 +273,39 @@ namespace Csharpagotchi
                 if (entity.Components.FirstOrDefault(c => c is PositionComponent) is PositionComponent positionComponent && entity.Components.FirstOrDefault(c => c is SpriteComponent) is SpriteComponent spriteComponent)
                 {
                     // Pega a posição do componente e atualiza a posição do sprite
-                    Canvas.SetLeft(spriteComponent.UIElement, positionComponent.X);
-                    Canvas.SetTop(spriteComponent.UIElement, positionComponent.Y);
+                    Canvas.SetLeft(spriteComponent.Image, positionComponent.X);
+                    Canvas.SetTop(spriteComponent.Image, positionComponent.Y);
+
+                    // Verifica se o estado de movimento mudou
+                    if (spriteComponent.IsMoving != spriteComponent.PreviousIsMoving)
+                    {
+                        // Se mudou, atualiza o estado anterior
+                        spriteComponent.PreviousIsMoving = spriteComponent.IsMoving;
+
+                        // Verifica se o slime está parado ou andando
+                        if (spriteComponent.IsMoving)
+                        {
+                            Console.WriteLine("Andando");
+                            BitmapImage image = new BitmapImage(); // Cria um objeto BitmapImage
+                            image.BeginInit();
+                            image.UriSource = new Uri(GetAbsolutePathFromRelativeUri("./assets/slime/sprites/walk150.gif"), UriKind.Absolute);
+                            image.EndInit();
+                            ImageBehavior.SetAnimatedSource(spriteComponent.Image, image);
+                            ImageBehavior.SetRepeatBehavior(spriteComponent.Image, RepeatBehavior.Forever);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Parado");
+                            // Se o slime estiver andando, define o sprite andando
+                            spriteComponent.Sprite = new Uri("./assets/slime/sprites/walk150.gif", UriKind.Relative);
+                        }
+                    }
 
                     // Aqui você poderia adicionar lógica de animação, etc.
                 }
             }
         }
-    }   
+    }
 
     /// <summary>
     /// Interação lógica para MainWindow.xam
@@ -159,36 +336,48 @@ namespace Csharpagotchi
             {
                 Interval = TimeSpan.FromMilliseconds(1000 / 60) // Define o intervalo para aproximadamente 60 FPS
             };
+
             gameTimer.Tick += GameLoop; // Define o método a ser chamado a cada tick
             gameTimer.Start(); // Inicia o temporizador
+
+            // Inicializa o jogo
+            StartGame();
         }
 
+        // Lista de entidades do jogo
         private readonly List<Entity> entities = new List<Entity>(); // Lista para armazenar os slimes massas
         private readonly EntityFactory entityFactory = new EntityFactory(); // Fábrica de entidades
 
-        private void OnMouseClick(object sender, MouseButtonEventArgs e)
-        {
-            // Obtém a posição atual do cursor do mouse
-            Point mousePosition = e.GetPosition(this);
+        // Lista de sistemas do jogo
+        private readonly List<object> systems = new List<object>(); // Lista para armazenar os sistemas do jogo
 
-            // "Spawna" um Slime na posição do cursor do mouse
-            entities.Add(entityFactory.CreateSlime(mousePosition.X, mousePosition.Y));
+        // Método inicial para o Game
+        private void StartGame()
+        {
+            // Cria um Slime na posição (0, 0)
+            entities.Add(entityFactory.CreateSlime(0, 0));
+
+            // Inicializa os sistemas
+            MovementSystem movementSystem = new MovementSystem();
+            RenderingSystem renderingSystem = new RenderingSystem();
+
+            // Adiciona os sistemas à lista de sistemas
+            systems.Add(movementSystem);
+            systems.Add(renderingSystem);
+
+            // Inicializa os sistemas
+            systems.ForEach(system => system.GetType().GetMethod("Start").Invoke(system, new object[] { entities, (Canvas)FindName("Game") }));
         }
 
+        // Método chamado a cada tick do DispatcherTimer
         private void GameLoop(object sender, EventArgs e)
         {
+            // Printar FPS no terminal
+            //Console.WriteLine("FPS: " + 1000 / gameTimer.Interval.TotalMilliseconds);
+
             // Lógica do jogo - atualizações, movimentos, cálculos, etc.
             // Chamadas de métodos para atualizar a lógica do jogo
-
-            // Por exemplo:
-            AtualizarPosicoes();
-            VerificarColisoes();
-
-            // Atualização da interface gráfica, se necessário
-            // Atualizações de elementos visuais com base nos estados do jogo
-
-            // Por exemplo:
-            AtualizarInterface();
+            systems.ForEach(system => system.GetType().GetMethod("Update").Invoke(system, new object[] { entities }));
         }
 
         // Métodos de exemplo para atualizar a lógica do jogo e a interface gráfica

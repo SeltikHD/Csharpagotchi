9c10df47c181445c0665f09f94ea8a96a437408b
Ewerthon Kawan <ewerthonkawan@gmail.com>
2023-12-05 23:43:08 -0300

    Sistema de arrastar finalizado | Inicio da física e angulação

--- Csharpagotchi/MainWindow.xaml.cs
+++ Csharpagotchi/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
@@ -21,17 +22,31 @@ namespace Csharpagotchi
         public double DistanceToChangeDirection { get; set; }
         public int SecondsToChangeDirection { get; set; }
         public bool IsMoving { get; set; } = false;
+        public bool IsGrabbing { get; set; } = false;
+    }
+
+    public class PhysicsComponent
+    {
+        // Objeto contendo as propriedades físicas do personagem
+        public bool Enabled { get; set; } = false;
+        public Vector Acceleration { get; set; } = new Vector(0, 0);
+        public double TimeInAcceleration { get; set; } = 0; // Tempo em ticks (60 ticks = 1 segundo)
+        public Vector LastPosition { get; set; } = new Vector(0, 0);
     }
 
     public class SpriteComponent
     {
         // Objeto contendo as Uri dos sprites do personagem parado e andando
+        public int Width { get; set; }
+        public int Height { get; set; }
         public Uri IdleSprite { get; set; }
         public BitmapImage IdleSpriteBitmap { get; set; }
         public Uri WalkSprite { get; set; }
         public BitmapImage WalkSpriteBitmap { get; set; }
         public Image Image { get; set; }
         public bool PreviousIsMoving { get; set; } = false;
+        public Point MousePosition { get; set; }
+        public double Angle { get; set; }
     }
 
     public class AudioComponent
@@ -67,10 +82,10 @@ namespace Csharpagotchi
         {
             Entity slime = new Entity();
 
-            Console.WriteLine(GetAbsolutePathFromRelativeUri("./assets/slime/sounds/walking"));
             slime.Components.Add(new MovementComponent { Position = new Vector(x, y) });
-            slime.Components.Add(new SpriteComponent { IdleSprite = new Uri(GetAbsolutePathFromRelativeUri("./assets/slime/sprites/walk150.gif")), WalkSprite = new Uri(GetAbsolutePathFromRelativeUri("./assets/slime/sprites/walk150.gif")) });
+            slime.Components.Add(new SpriteComponent { Width = 98, Height = 98, IdleSprite = new Uri(GetAbsolutePathFromRelativeUri("./assets/slime/sprites/walk150.gif")), WalkSprite = new Uri(GetAbsolutePathFromRelativeUri("./assets/slime/sprites/walk150.gif")) });
             slime.Components.Add(new AudioComponent { WalkingAudioFolder = GetAbsolutePathFromRelativeUri("./assets/slime/sounds/walking") });
+            slime.Components.Add(new PhysicsComponent());
 
             return slime;
         }
@@ -79,11 +94,64 @@ namespace Csharpagotchi
     // Sistema
     interface ISystem
     {
+        // Entidades; Canvas; Função para setar o cursor
         void Start(List<Entity> entities, Canvas canvas);
         void Update(List<Entity> entities);
         void UpdatePerSecond(List<Entity> entities);
     }
 
+    // Sistema de física
+    public class PhysicsSystem : ISystem
+    {
+        public void Start(List<Entity> entities, Canvas _canvas)
+        {
+            // Lógica de inicialização, se necessário
+        }
+
+        public void Update(List<Entity> entities)
+        {
+            foreach (var entity in entities)
+            {
+                if (entity.Components.FirstOrDefault(c => c is PhysicsComponent) is PhysicsComponent physicsComponent && entity.Components.FirstOrDefault(c => c is SpriteComponent) is SpriteComponent spriteComponent)
+                {
+                    // Verifica se o componente está habilitado
+                    if (physicsComponent.Enabled)
+                    {
+                        if(physicsComponent.TimeInAcceleration <= 0)
+                        {
+                            // Atualiza a última posição
+                            physicsComponent.LastPosition = (Vector)spriteComponent.MousePosition;
+                            physicsComponent.TimeInAcceleration = 0;
+                        } 
+                        else
+                        {
+                            // Atualiza o deslocamento e salvando a última posição
+                            //physicsComponent.LastPosition += (Vector)spriteComponent.MousePosition;
+                        }
+
+                        // Atualiza o tempo de aceleração
+                        physicsComponent.TimeInAcceleration += 1;
+                    }
+                    else if (physicsComponent.TimeInAcceleration > 0)
+                    {
+                        // Calcula a aceleração
+                        physicsComponent.Acceleration = (physicsComponent.LastPosition - (Vector)spriteComponent.MousePosition) / (physicsComponent.TimeInAcceleration / 60);
+                        Console.WriteLine(physicsComponent.Acceleration);
+
+                        // Atualiza o tempo de aceleração
+                        physicsComponent.TimeInAcceleration = 0;
+                    }
+                }
+            }
+        }
+
+        public void UpdatePerSecond(List<Entity> entities)
+        {
+            // Lógica de atualização por segundo, se necessário
+        }
+    }
+
+    // Sistema de áudio
     public class AudioSystem : ISystem
     {
         private readonly Random random = new Random();
@@ -108,7 +176,7 @@ namespace Csharpagotchi
             // Se não houver arquivos de áudio na pasta, não faz nada
         }
 
-        public void Start(List<Entity> entities, Canvas canvas)
+        public void Start(List<Entity> entities, Canvas _canvas)
         {
             // Criar um MediaPlayer para cada entidade que possuir um AudioComponent
             foreach (var entity in entities)
@@ -138,7 +206,6 @@ namespace Csharpagotchi
                     }
                 }
             }
-
         }
     }
 
@@ -168,16 +235,50 @@ namespace Csharpagotchi
             {
                 if (entity.Components.FirstOrDefault(c => c is MovementComponent) is MovementComponent movementComponent)
                 {
-                    // Atualiza a posição com base na velocidade
-                    if (movementComponent.IsMoving) {
-                        movementComponent.Position += movementComponent.Speed;
+                    SpriteComponent spriteComponent = (SpriteComponent)entity.Components.FirstOrDefault(c => c is SpriteComponent);
+
+                    if (movementComponent.IsGrabbing)
+                    {
+                        if (spriteComponent != null)
+                        {
+                            double radians = spriteComponent.Angle * (Math.PI / 180); // Converte o ângulo de graus para radianos
+                            double sin = Math.Sin(radians);
+                            double cos = Math.Cos(radians);
+
+                            double offsetX = spriteComponent.Width / 2; // Deslocamento horizontal do centro da imagem
+                            double offsetY = spriteComponent.Height / 2; // Deslocamento vertical do centro da imagem
+
+                            // Calcula a posição da imagem ajustando com base no ângulo de rotação
+                            double rotatedX = cos * offsetX - sin * offsetY;
+                            double rotatedY = sin * offsetX + cos * offsetY;
+
+                            double imageX = spriteComponent.MousePosition.X - rotatedX; // Calcula a nova posição X da imagem
+                            double imageY = spriteComponent.MousePosition.Y - rotatedY; // Calcula a nova posição Y da imagem
+
+                            movementComponent.Position = new Vector(imageX, imageY);
+
+                            //movementComponent.Position = new Vector(spriteComponent.MousePosition.X - spriteComponent.Width / 2, spriteComponent.MousePosition.Y - spriteComponent.Height / 2);
+                        }
+
+                        movementComponent.IsMoving = false;
+                    }
+                    else
+                    {
+                        // Atualiza a posição com base na velocidade
+                        if (movementComponent.IsMoving)
+                        {
+                            movementComponent.Position += movementComponent.Speed;
+                        }
+
+                        // Atualiza a velocidade aleatoriamente
+                        SetRandomMovement(movementComponent);
                     }
 
-                    // Verifica se o slime está prestes a sair da tela
-                    CheckScreenBounds(movementComponent, canvas);
-
-                    // Atualiza a velocidade aleatoriamente
-                    SetRandomMovement(movementComponent);
+                    if (spriteComponent != null)
+                    {
+                        // Verifica se o slime está prestes a sair da tela
+                        CheckScreenBounds(movementComponent, spriteComponent, canvas);
+                    }
                 }
             }
         }
@@ -194,15 +295,11 @@ namespace Csharpagotchi
             }
         }
 
-        private void CheckScreenBounds(MovementComponent movementComponent, Canvas canvas)
+        private void CheckScreenBounds(MovementComponent movementComponent, SpriteComponent spriteComponent, Canvas canvas)
         {
             // Obtém as dimensões da tela
             double screenWidth = canvas.ActualWidth;
             double screenHeight = canvas.ActualHeight;
-
-            // Obtém as dimensões do sprite
-            double spriteWidth = 98;
-            double spriteHeight = 98;
 
             // Verifica se o slime está fora dos limites da tela e ajusta a posição
             if (movementComponent.Position.X < 0)
@@ -210,9 +307,9 @@ namespace Csharpagotchi
                 movementComponent.Position = new Vector(0, movementComponent.Position.Y);
                 movementComponent.Speed = new Vector(Math.Abs(movementComponent.Speed.X), movementComponent.Speed.Y); // Inverte a direção
             }
-            else if (movementComponent.Position.X + spriteWidth > screenWidth)
+            else if (movementComponent.Position.X + spriteComponent.Width > screenWidth)
             {
-                movementComponent.Position = new Vector(screenWidth - spriteWidth, movementComponent.Position.Y);
+                movementComponent.Position = new Vector(screenWidth - spriteComponent.Width, movementComponent.Position.Y);
                 movementComponent.Speed = new Vector(-Math.Abs(movementComponent.Speed.X), movementComponent.Speed.Y); // Inverte a direção
             }
 
@@ -221,9 +318,9 @@ namespace Csharpagotchi
                 movementComponent.Position = new Vector(movementComponent.Position.X, 0);
                 movementComponent.Speed = new Vector(movementComponent.Speed.X, Math.Abs(movementComponent.Speed.Y)); // Inverte a direção
             }
-            else if (movementComponent.Position.Y + spriteHeight > screenHeight)
+            else if (movementComponent.Position.Y + spriteComponent.Height > screenHeight)
             {
-                movementComponent.Position = new Vector(movementComponent.Position.X, screenHeight - spriteHeight);
+                movementComponent.Position = new Vector(movementComponent.Position.X, screenHeight - spriteComponent.Height);
                 movementComponent.Speed = new Vector(movementComponent.Speed.X, -Math.Abs(movementComponent.Speed.Y)); // Inverte a direção
             }
         }
@@ -277,11 +374,68 @@ namespace Csharpagotchi
                     spriteComponent.Image = new Image
                     {
                         Source = spriteComponent.IdleSpriteBitmap,
-                        Height = 98,
-                        Width = 98
+                        Height = spriteComponent.Height,
+                        Width = spriteComponent.Width
                     };
 
                     canvas.Children.Add(spriteComponent.Image);
+                    canvas.MouseMove += (sender, e) =>
+                    {
+                        spriteComponent.MousePosition = e.GetPosition(canvas);
+
+                        if (e.LeftButton == MouseButtonState.Pressed)
+                        {
+                            spriteComponent.Angle = Math.Atan2(spriteComponent.MousePosition.Y, spriteComponent.MousePosition.X) * (180 / Math.PI);
+
+                            RotateTransform rotateTransform = new RotateTransform(spriteComponent.Angle);
+                            spriteComponent.Image.RenderTransform = rotateTransform;
+                        }
+                    };
+
+                    spriteComponent.Image.MouseMove += (sender, e) =>
+                    {
+                        if (entity.Components.FirstOrDefault(c => c is MovementComponent) is MovementComponent movementComponent)
+                        {
+                            if (e.LeftButton == MouseButtonState.Pressed)
+                            {
+                                if (entity.Components.FirstOrDefault(c => c is PhysicsComponent) is PhysicsComponent physicsComponent)
+                                {
+                                    physicsComponent.Enabled = true;
+                                }
+
+                                canvas.CaptureMouse();
+
+                                Mouse.OverrideCursor = Cursors.SizeAll;
+                                canvas.Background = new SolidColorBrush(Color.FromArgb(1, 0, 0, 0));
+
+                                movementComponent.IsGrabbing = true;
+                            } else
+                            {
+                                Mouse.OverrideCursor = Cursors.Hand;
+                            }
+                        }
+                    };
+
+                    canvas.MouseUp += (sender, e) =>
+                    {
+                        if (entity.Components.FirstOrDefault(c => c is MovementComponent) is MovementComponent movementComponent && movementComponent.IsGrabbing)
+                        {
+                            spriteComponent.MousePosition = new Point();
+
+                            if (entity.Components.FirstOrDefault(c => c is PhysicsComponent) is PhysicsComponent physicsComponent)
+                            {
+                                physicsComponent.Enabled = false;
+                            }
+
+                            movementComponent.IsGrabbing = false;
+
+                            RotateTransform rotateTransform = new RotateTransform(0);
+                            spriteComponent.Image.RenderTransform = rotateTransform;
+
+                            canvas.ReleaseMouseCapture();
+                            canvas.Background = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+                        }
+                    };
                 }
             }
         }
@@ -328,7 +482,7 @@ namespace Csharpagotchi
     }
 
     /// <summary>
-    /// Interação lógica para MainWindow.xam
+    /// Interação lógica para MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -391,11 +545,13 @@ namespace Csharpagotchi
             MovementSystem movementSystem = new MovementSystem();
             RenderingSystem renderingSystem = new RenderingSystem();
             AudioSystem audioSystem = new AudioSystem();
+            PhysicsSystem physicsSystem = new PhysicsSystem();
 
             // Adiciona os sistemas à lista de sistemas
             systems.Add(movementSystem);
             systems.Add(renderingSystem);
             systems.Add(audioSystem);
+            systems.Add(physicsSystem);
 
             // Inicializa os sistemas
             systems.ForEach(system => system.GetType().GetMethod("Start").Invoke(system, new object[] { entities, (Canvas)FindName("Game") }));
